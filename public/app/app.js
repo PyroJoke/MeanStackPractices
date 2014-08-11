d85769634daa11aa344145f773c1c9001e692724
Dmitry Mikhaylov <dmitry.pyrojoke.mikhaylov@gmail.com>
2014-08-12 01:05:54 +0300

    added some UI

--- public/app/app.js
+++ public/app/app.js
@@ -3,9 +3,5 @@ var module = angular.module('app', ['ngResource', 'ngRoute']);
 module.config(['$routeProvider','$locationProvider',function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-        .when('/', {templateUrl: '/partials/main', controller: 'mainCtrl'});
-}]);
-
-module.controller('mainCtrl', ['$scope', function($scope) {
-    $scope.myVar = 'Hello from angular app';
+        .when('/', {templateUrl: '/partials/main', controller: 'mvMainCtrl'});
 }]);
